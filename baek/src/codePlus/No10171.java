9f6b27f9586e10573dd23ffc270afc742c6cd40d
Administrator <Administrator@DESKTOP-TBTNGER>
2023-05-31 00:20:49 +0900

    백준 10171 고양이

---
+++ baek/src/codePlus/No10171.java
@@ -0,0 +1,15 @@
+package codePlus;
+
+import java.util.Scanner;
+
+public class No10171 {
+    public static void main(String[] args) {
+    	
+    	System.out.println("\\    /\\"
+    			+ "\n )  ( ')"
+    			+ "\n(  /  )"
+    			+ "\n \\(__)|");
+    	 
+    }
+}
+
