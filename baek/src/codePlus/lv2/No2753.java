c13ab13b849e16715ced3134cd3c8c0aaca7f222
Administrator <Administrator@DESKTOP-TBTNGER>
2023-06-14 00:47:17 +0900

    No2753

---
+++ baek/src/codePlus/lv2/No2753.java
@@ -0,0 +1,24 @@
+package codePlus.lv2;
+
+import java.util.Scanner;
+
+public class No2753 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+
+		int result = 0;
+
+		if ((a % 4 == 0 && a % 100 > 0) || a % 400 == 0) {
+			result = 1;
+		} else {
+			result = 0;
+		}
+
+		System.out.print(result);
+
+	}
+
+}
