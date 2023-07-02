a6b6721c97214661124d27f5f058f0f6433a3be3
ruden1303 <ygzzang97@gmail.com>
2023-07-02 19:35:04 +0900

    No1152

---
+++ baek/src/tear/Bronze2/No1152.java
@@ -0,0 +1,20 @@
+package tear.Bronze2;
+
+import java.util.Scanner;
+
+public class No1152 {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        String[] result = str.trim().split(" ");
+
+        if (str.trim().isEmpty() || str.trim().equals(""))
+            System.out.println(0);
+        else
+            System.out.print(result.length);
+    }
+}
