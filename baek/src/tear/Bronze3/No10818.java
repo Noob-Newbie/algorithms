7d63e8a0efb150d39bfed0fe390233d71906a940
ruden1303 <ygzzang97@gmail.com>
2023-07-03 22:00:02 +0900

    No10818 최대 최소 스트림 써서 찾았음

---
+++ baek/src/tear/Bronze3/No10818.java
@@ -0,0 +1,26 @@
+package tear.Bronze3;
+
+import java.io.IOException;
+import java.util.*;
+
+public class No10818 {
+
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+
+        int cnt = sc.nextInt();
+
+        List<Integer> numList = new ArrayList<>();
+
+        for (int i = 0; i < cnt; i++) {
+            numList.add(sc.nextInt());
+        }
+
+        int a = numList.stream().sorted().findFirst().get();
+        int b = numList.stream().sorted(Comparator.reverseOrder()).findFirst().get();
+
+        System.out.println(a + " " + b);
+    }
+}
