7662591bfca4f95f42187a6ccd690a2836ca0a41
ruden1303 <ygzzang97@gmail.com>
2023-07-03 21:50:40 +0900

    No2675 BufferReader, StringTokenizer, StringBuilder 사용법 공부중

---
+++ baek/src/tear/Bronze2/No2675.java
@@ -0,0 +1,34 @@
+package tear.Bronze2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No2675 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int cnt = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < cnt; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int cnt2 = Integer.parseInt(st.nextToken());
+
+            String str[] = st.nextToken().split("");
+            for(String string : str){
+                for (int j = 0; j < cnt2; j++) {
+                    sb.append(string);
+                }
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
